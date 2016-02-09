1128efb4b64795086f9f29bb28037b30a77c0f3d
David Ellis <davidkellis@gmail.com>
2016-02-08 23:25:50 -0600

    small change

--- src/main/scala/com/github/davidkellis/seven/domain/Simulator.scala
+++ src/main/scala/com/github/davidkellis/seven/domain/Simulator.scala
@@ -4,7 +4,7 @@ import com.github.davidkellis.seven.Time
 
 class Simulator() {
   def run(trial: Trial): Unit = {
-    val tradingDays: Seq[Time.Datestamp] = ???
+    val tradingDays = TradingSchedule.tradingDays(trial.startTime.toLocalDate, trial.endTime.toLocalDate, TradingSchedule.defaultTradingSchedule(_))
     val defaultTradingHours: Array[Time.Timestamp] = ???
   }
 }
