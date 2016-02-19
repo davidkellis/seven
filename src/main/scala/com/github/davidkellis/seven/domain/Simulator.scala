ea0b3a1200b03e6603f349fa0e0e7a97cad48c83
David Ellis <davidkellis@gmail.com>
2016-02-18 23:48:24 -0600

    no time; starting to implement adjustments to account portfolios, open orders, etc. for splits and dividends that take effect between successive trading opportunities that the Simulator gives to the strategy

--- src/main/scala/com/github/davidkellis/seven/domain/Simulator.scala
+++ src/main/scala/com/github/davidkellis/seven/domain/Simulator.scala
@@ -5,7 +5,7 @@ import com.github.davidkellis.seven.domain.CoreTypes.FillPriceFn
 import org.joda.time.DateTime
 
 class Simulator() {
-  def run(exchanges: Seq[Exchange], strategy: Strategy, trial: Trial): Unit = {
+  def run(exchanges: Seq[Exchange], accounts: Seq[BrokerageAccount], strategy: Strategy, trial: Trial): Unit = {
     val tradingDays = TradingSchedule.tradingDays(trial.startTime.toLocalDate, trial.endTime.toLocalDate, TradingSchedule.defaultTradingSchedule(_))
     val tradingTimes = trial.dailyTradingTimes
     var currentTime = tradingDays.head.toDateTime(tradingTimes.head, Time.EasternTimeZone)
@@ -15,8 +15,7 @@ class Simulator() {
         previousTime = currentTime
         currentTime = localDate.toDateTime(localTime, Time.EasternTimeZone)
 
-        // todo: adjust (1) all open orders and order cancellations tracked by the exchanges, as well as (2) the portfolio managed by the strategy
-        // for splits and dividends that have gone into effect at some point within the interval (previousTime, currentTime]
+        adjustWorldForRecentSplitsAndDividends(exchanges, accounts, previousTime, currentTime)
 
         // todo: log the current value of the portfolio managed by the strategy
         strategy.evaluate(previousTime, currentTime, CoreTypes.ReEvaluate)
@@ -24,6 +23,11 @@ class Simulator() {
         fillOrders(exchanges, trial.fillPriceFn, previousTime, currentTime)
       }
     }
+  }
+
+  def adjustWorldForRecentSplitsAndDividends(exchanges: Seq[Exchange], accounts: Seq[BrokerageAccount], previousTime: DateTime, currentTime: DateTime): Unit = {
+    // todo: adjust (1) all open orders and order cancellations tracked by the exchanges, as well as (2) the portfolio managed by the strategy
+    // for splits and dividends that have gone into effect at some point within the interval (previousTime, currentTime]
   }
 
   def fillOrders(exchanges: Seq[Exchange], fillPriceFn: FillPriceFn, previousTime: DateTime, currentTime: DateTime): Unit = {
